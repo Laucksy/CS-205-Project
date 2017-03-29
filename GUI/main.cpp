45506790cde58c215996fd2606c1ce5203548c7a
drewCarletonLaptop <carletod@lafayette.edu>
2017-03-29 13:23:35 -0400

    fixed parse

--- GUI/main.cpp
+++ GUI/main.cpp
@@ -18,6 +18,7 @@ int main(int argc, char *argv[])
     w.show();
 
 
+
     Code x;
     //vector<string> v;
    vector<string> v = x.parse("/Users/drewcarleton/Project205/axolotl/GUI/Student.java");
