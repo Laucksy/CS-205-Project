3d9b9204aa2e09cedb454bf0053f65881a539802
Shira <weins@lafayette.edu>
2017-04-16 14:03:43 -0400

    testing find in git

--- git/main.cpp
+++ git/main.cpp
@@ -8,14 +8,14 @@ int main(int argc, char *argv[])
     argc = 0;
     argv = nullptr;
 
-    cout << "Hello World!" << endl;
-    Git::init();
+   // cout << "Hello World!" << endl;
+    //Git::init();
     //Git::add_file("test.txt");
     //Git::add_file("TestDB");
     //cout << Git::status() << endl;
     //system("clear");
     //cout << Git::pull() << endl;
-    cout << Git::push() << endl;
+    //cout << Git::push() << endl;
     //cout << Git::reset() << endl;
     Git* test;
     cout << test->find_file_path("exampleImportant.pages") << endl;
